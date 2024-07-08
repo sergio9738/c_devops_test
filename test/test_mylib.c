6976e5e992da1379b4c311d6e55c43d76f8b621b
jdiazfernandez <yesica.diaz@upm.es>
2024-07-08 17:53:58 +0200

    Fix Test - Update test_mylib.c

--- test/test_mylib.c
+++ test/test_mylib.c
@@ -16,7 +16,7 @@ void test_add(void) {
 }
 
 void test_subtract(void) {
-    TEST_ASSERT_EQUAL(1, subtract(4, 2));
+    TEST_ASSERT_EQUAL(1, subtract(3, 2));
     TEST_ASSERT_EQUAL(2, subtract(1, -1));
     TEST_ASSERT_EQUAL(0, subtract(-1, -1));
 }
